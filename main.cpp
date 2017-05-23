2595acc7a7b386e663672423de361679a996d3a9
Alexandr <aleksandr_livitski@mail.ru>
2017-05-23 16:25:49 +0700

    Add two dinamic arrays

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+	setlocale (LC_ALL, "rus");
+	int N = 4;
+	int *mascomputer = new int[N];
+	int *masgamer = new int[B];
+	delete[] mascomputer;
+	delete[] masgamer;
+	return 0;
+}
